871839ce5d1a783a9af81d0b4fcf75d41cab76a8
Casey Scarborough <caseyscarborough@gmail.com>
2014-12-29 08:23:57 -0500

    Initial Commit

---
+++ RedditClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditClient
+//
+//  Created by Casey Scarborough on 12/29/14.
+//  Copyright (c) 2014 Casey Scarborough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditClient
+//
+//  Created by Casey Scarborough on 12/29/14.
+//  Copyright (c) 2014 Casey Scarborough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
